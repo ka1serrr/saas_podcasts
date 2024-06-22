8b3dbd23fac29b76e32751eaf7e428633cefa319
Yaroslav Yasinskii <yroslav070411@gmail.com>
2024-06-22 19:36:16 +0300

    Фича: добавлено базовое отображение страниц

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,17 @@
 import { LeftSideBar, RightSideBar, MobileNav } from "@/app/layouts";
+import { ReactNode } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <div className='flex flex-col'>
-      <main className='flex bg-black-1'>
+      <main className='flex bg-black-3'>
         <LeftSideBar />
 
-        <section className='border-2 border-red-500 flex min-h-screen flex-1 flex-col px-4 sm:px-14'>
-          
+        <section className='flex min-h-screen flex-1 flex-col px-4 sm:px-14'>
           <div className='mx-auto flex w-full max-w-5xl flex-col max-sm:px-4'>
             <MobileNav />
 
