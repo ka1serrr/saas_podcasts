901eab34ba42f673bc1bdad22d8f5e2d469a53c7
Yaroslav Yasinskii <yroslav070411@gmail.com>
2024-06-22 17:14:41 +0300

    Фикс: корректное отображение лого и названия в меню
    - Добавил свойство flex, теперь норм отображается

--- appRoot/layouts/leftSideBar/LeftSideBar.tsx
+++ appRoot/layouts/leftSideBar/LeftSideBar.tsx
@@ -10,7 +10,7 @@ export const LeftSideBar = () => {
   return (
     <aside className={styles.left_sidebar}>
       <nav className={styles.navigation}>
-        <Link href={paths.home}>
+        <Link href={paths.home} className={styles.title_link}>
           <Image src='/icons/logo.svg' alt='logo' width={23} height={27} />
           <h1 className={styles.title}>Home</h1>
         </Link>
