e38ab37e5873d3e4d1e853dc3c2aeef06234d21d
Yaroslav Yasinskii <yroslav070411@gmail.com>
2024-06-22 19:29:29 +0300

    Фича: добавить базовое отображение правого сайдбара

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import { LeftSideBar } from "@/app/layouts";
+import { LeftSideBar, RightSideBar, MobileNav } from "@/app/layouts";
 
 export default function RootLayout({
   children,
@@ -6,11 +6,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
+    <div className='flex flex-col'>
+      <main className='flex bg-black-1'>
         <LeftSideBar />
-        {children}
-        <p>Right sideBar</p>
+
+        <section className='border-2 border-red-500 flex min-h-screen flex-1 flex-col px-4 sm:px-14'>
+          
+          <div className='mx-auto flex w-full max-w-5xl flex-col max-sm:px-4'>
+            <MobileNav />
+
+            <div className='flex flex-col md:pb-14'>
+              Toaster
+              {children}
+            </div>
+          </div>
+        </section>
+
+        <RightSideBar />
       </main>
     </div>
   );
