2192f39f79ea8378252919324a893705e45f1e01
Yaroslav Yasinskii <yroslav070411@gmail.com>
2024-06-22 15:27:17 +0300

    Фикс: пофикшена ошибка с hydration
    -Link оборачивал другой Link компонент, из-за чего возникала ошибка

--- appRoot/layouts/leftSideBar/LeftSideBar.tsx
+++ appRoot/layouts/leftSideBar/LeftSideBar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { paths } from "@/app/config";
-
 import styles from "./leftSideBar.module.scss";
 import Link from "next/link";
 import Image from "next/image";
@@ -13,9 +12,7 @@ export const LeftSideBar = () => {
       <nav className={styles.navigation}>
         <Link href={paths.home}>
           <Image src='/icons/logo.svg' alt='logo' width={23} height={27} />
-          <Link href={paths.home}>
-            <h1 className={styles.title}>Home</h1>
-          </Link>
+          <h1 className={styles.title}>Home</h1>
         </Link>
 
         <LeftSideBarLinks />
